be78843b965e70e8590a71fd830f75b268f63f5c
KhantParthA <parthkhant1@outlook.com>
2019-12-12 22:41:02 +0530

    initcommit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import './css/app.css'
 
-const Count = props => {
+const Count = prop => {
   return (
     <div>
       <div id="block1">
